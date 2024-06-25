e1474732e1e93c14f1e060ec8d7a3aa809181838
iva <ivalulyanov1@gmail.com>
2024-06-25 21:02:31 +0300

    refactor

--- backends/settings.py
+++ backends/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '147.45.146.114']
 
 # Application definition
 
