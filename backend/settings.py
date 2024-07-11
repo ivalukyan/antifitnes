a4fbef6e6a0c02f51aa718640e9a06ad4eb0f1b7
ivalukyan <ivalukyanov@gmail.com>
2024-07-11 14:55:40 +0300

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': POSTGRES_DB,
         'USER': POSTGRES_USER,
         'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': 'db',
+        'HOST': POSTGRES_HOST,
         'PORT': POSTGRES_PORT
     }
 }
