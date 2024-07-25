aa44fec81cddee0c0974de14aa84deac5b8589dc
iva <ivalulyanov1@gmail.com>
2024-07-26 00:44:03 +0300

    fix

--- src/db/router.py
+++ src/db/router.py
@@ -2,5 +2,6 @@ import psycopg2
 from env import POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST
 
 
-conn = psycopg2.connect(f"dbname={POSTGRES_DB} user={POSTGRES_USER} host={POSTGRES_HOST} password={POSTGRES_PASSWORD}")
+# conn = psycopg2.connect(f"dbname={POSTGRES_DB} user={POSTGRES_USER} host={POSTGRES_HOST} password={POSTGRES_PASSWORD}")
+conn = psycopg2.connect('postgresql://gen_user:W%2CMkd)Q4VPm0X%3C@147.45.247.128:5432/default_db')
 cursor = conn.cursor()
