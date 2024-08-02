836ace6fc2914e2c3ac9a9502848c6eebbacb653
iva <ivalulyanov1@gmail.com>
2024-08-02 16:49:46 +0300

    fix

--- src/db/db_profile.py
+++ src/db/db_profile.py
@@ -111,7 +111,7 @@ async def get_all_phones(phone_number: str) -> bool:
     cursor.execute("""SELECT phone_number FROM bot_app_profile""")
     result = cursor.fetchall()
     if result is not None:
-        print(result)
+        print(result[0])
         if phone_number in result[0]:
             return True
         else:
